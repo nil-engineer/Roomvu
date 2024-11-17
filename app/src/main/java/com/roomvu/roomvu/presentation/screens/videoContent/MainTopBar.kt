e5e51ce813d1b722edcd0c4778bf5d7f2f23dac1
Niloufar <niloufar2012shabestari@gmail.com>
2024-11-17 16:58:22 +0330

    implemented delete video confirmation dialog

--- app/src/main/java/com/roomvu/roomvu/presentation/screens/videoContent/MainTopBar.kt
+++ app/src/main/java/com/roomvu/roomvu/presentation/screens/videoContent/MainTopBar.kt
@@ -4,12 +4,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -22,12 +25,23 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainTopBar() {
     var expanded by remember { mutableStateOf(false) }
+    var openAlertDialog by remember { mutableStateOf(false) }
+    val mainViewModel = MainViewModel()
+    val dialogState = mainViewModel.openDialog.value
 
+    val onDeleteClicked: () -> Unit = {
+        mainViewModel.updateDialogState(true)
+    }
+
+    if(dialogState){
+        DeleteConfirmAlert()
+    }
     TopAppBar(title = {
         Row(
             verticalAlignment = Alignment.CenterVertically,
@@ -58,8 +72,70 @@ fun MainTopBar() {
                     })
                     DropdownMenuItem(text = { Text("Delete") }, onClick = {
                         expanded = false
+                        onDeleteClicked()
                     })
                 }
             }
         })
+}
+
+@Composable
+fun DeleteConfirmAlert() {
+    val openDialog = remember { mutableStateOf(false)  }
+    val mainViewModel = MainViewModel()
+//    val dialogState = mainViewModel.openDialog.value
+    AlertDialog(
+        containerColor = AlertDialogDefaults.containerColor,
+        onDismissRequest = {
+//            openDialog.value = false
+            mainViewModel.updateDialogState(false)
+        },
+        title = {
+            Text(text = "Delete post",
+                fontFamily = FontFamily.SansSerif,
+                fontWeight = FontWeight(590),
+                textAlign = TextAlign.Center,
+                color = Color.Black
+            )
+        },
+        text = {
+            Text("Are you sure you want to delete this post?",
+                fontFamily = FontFamily.SansSerif,
+                fontWeight = FontWeight(400),
+                textAlign = TextAlign.Center,
+                color = Color.Black)
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+//                    openDialog.value = false
+                    mainViewModel.updateDialogState(false)
+
+                }) {
+                Text("Delete",
+                    fontFamily = FontFamily.SansSerif,
+                    fontWeight = FontWeight(400),
+                    color = Color.Black)
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+//                    openDialog.value = false
+                    mainViewModel.updateDialogState(false)
+
+                }) {
+                Text("Cancel",
+                    fontFamily = FontFamily.SansSerif,
+                    fontWeight = FontWeight(400),
+                    color = Color.Black)
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun PreviewAlert() {
+//    DeleteConfirmAlert()
 }
