684fea3273e221f62adc59c3b7bd3df8edecb699
Niloufar <niloufar2012shabestari@gmail.com>
2024-11-18 12:48:34 +0330

    added fromurlencoded to update video api

--- app/src/main/java/com/roomvu/roomvu/data/remote/ChallengeApi.kt
+++ app/src/main/java/com/roomvu/roomvu/data/remote/ChallengeApi.kt
@@ -2,6 +2,7 @@ package com.roomvu.roomvu.data.remote
 
 import com.roomvu.roomvu.domain.entities.networkEntities.VideoEntity
 import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.PUT
 
@@ -9,7 +10,8 @@ interface ChallengeApi {
     @GET("v1/dev/android-challenge/video")
     suspend fun getVideo(): VideoEntity
 
-    @PUT("v1/dev/android-challenge/video?title")
+    @FormUrlEncoded
+    @PUT("v1/dev/android-challenge/video")
     suspend fun updateVideo(
         @Field("title") title: String,
         @Field("description") description: String
