83e83cc33a46f0b5082663891434e443638c7f6c
Niloufar <niloufar2012shabestari@gmail.com>
2024-11-18 15:14:43 +0330

    handle progress bar state in viewmodel

--- app/src/main/java/com/roomvu/roomvu/presentation/screens/videoContent/MainViewModel.kt
+++ app/src/main/java/com/roomvu/roomvu/presentation/screens/videoContent/MainViewModel.kt
@@ -1,9 +1,14 @@
 package com.roomvu.roomvu.presentation.screens.videoContent
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.roomvu.roomvu.connectivity.ConnectivityObserver
+import com.roomvu.roomvu.connectivity.NetworkConnectivityObserver
 import com.roomvu.roomvu.domain.entities.networkEntities.VideoEntity
 import com.roomvu.roomvu.domain.usecases.GetVideoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -13,23 +18,46 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val getVideoUseCase: GetVideoUseCase) :
+class MainViewModel @Inject constructor(
+    private val connectivity: NetworkConnectivityObserver,
+    private val getVideoUseCase: GetVideoUseCase
+) :
     ViewModel() {
     private val _openDialog = MutableStateFlow(false)
     val openDialog = _openDialog.asStateFlow()
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading = _isLoading.asStateFlow()
 
     private val _video = MutableLiveData<VideoEntity>()
     var video: LiveData<VideoEntity> = _video
-    var time = ""
+    private var time = ""
+    private var network by mutableStateOf(ConnectivityObserver.Status.Available)
+
+    init {
+        fetchVideo()
+        viewModelScope.launch {
+            connectivity.observe().collect {
+                network = it
+            }
+        }
+    }
+
     fun updateDialogState(state: Boolean) {
         _openDialog.value = state
     }
 
-    fun fetchVideo() {
-        viewModelScope.launch {
-            val vid = getVideoUseCase.getVideo()
-            _video.value = vid
+    private fun fetchVideo() {
+        if (network == ConnectivityObserver.Status.Available) {
+            viewModelScope.launch {
+                val vid = getVideoUseCase.getVideo()
+                _video.value = vid
+            }
+            _isLoading.value = false
         }
+        else {
+            _isLoading.value = true
+        }
+
     }
 
     fun getTimePart(): String {
@@ -40,4 +68,5 @@ class MainViewModel @Inject constructor(private val getVideoUseCase: GetVideoUse
         }
         return time
     }
+
 }
