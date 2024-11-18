df838be46235c922b6c8e041ec1b9345b3a8224b
Niloufar <niloufar2012shabestari@gmail.com>
2024-11-18 17:59:13 +0330

    adjust width of video player

--- app/src/main/java/com/roomvu/roomvu/presentation/screens/components/VideoPlayer.kt
+++ app/src/main/java/com/roomvu/roomvu/presentation/screens/components/VideoPlayer.kt
@@ -7,11 +7,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.ui.PlayerControlView
 import androidx.media3.ui.PlayerView
 import com.roomvu.roomvu.R
 
@@ -19,7 +17,9 @@ import com.roomvu.roomvu.R
 @Composable
 fun VideoPlayer(player: ExoPlayer?) {
     AndroidView(
-        modifier = Modifier.height(dimensionResource(id = R.dimen.player_height).value.dp),
+        modifier = Modifier
+            .height(dimensionResource(id = R.dimen.player_height).value.dp)
+            .fillMaxWidth(),
         factory = { context ->
             PlayerView(context).apply {
                 this.player = player
